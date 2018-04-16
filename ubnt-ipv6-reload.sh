bfaa9b45c46d143887cb714e0aab6d609458db36
Roy Olav Purser <admin@royolav.net>
2018-04-16 16:25:55 +0200

    fix ping6 path

--- ubnt-ipv6-reload.sh
+++ ubnt-ipv6-reload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TEST_IP="2001:4860:4860::8888"
-PING_RES=`/bin/ping -w 5 -c 3  ${TEST_IP} | grep --count "100%"`
+PING_RES=`/bin/ping6 -w 5 -c 3  ${TEST_IP} | grep --count "100%"`
 PREFIX="/opt/vyatta/bin/vyatta-op-cmd-wrapper"
 
 if [ "${PING_RES}" -eq "1" ]
@@ -8,5 +8,7 @@ then
     ${PREFIX} release dhcpv6-pd interface eth0
     ${PREFIX} delete dhcpv6-pd duid 
     ${PREFIX} renew dhcpv6-pd interface eth0
+else
+    print "IPv6 detected"
 fi
     
